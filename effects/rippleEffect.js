88eba1e3c1493d13875127f6ac77b9de9d66546a
Raghav Misra <raghav.m2014@gmail.com>
2021-03-01 11:35:30 -0800

    Write some effects

---
+++ effects/rippleEffect.js
@@ -0,0 +1,47 @@
+window._currentEffect = params => ({
+    html: /*html*/`
+    <div class="circle">
+        <div class="circle">
+            <div class="circle"></div>
+        </div>
+    </div>
+    `,
+    css: /*css*/`
+        main .circle {
+            transform: scale(0);
+            border-radius: 50%;
+            border: 10px white solid;
+            display: flex;
+            box-shadow: 0 0 50px white;
+            justify-content: center;
+            align-items: center;
+            animation: big 1s ease-in-out;
+        }
+        
+        main > .circle {
+            width: 2000px;
+            height: 2000px;
+        }
+        
+        main > .circle > .circle {
+            width: 1500px;
+            height: 1500px;
+        }
+        
+        main > .circle > .circle > .circle {
+            width: 500px;
+            height: 500px;
+        }
+        
+        @keyframes big {
+            from {
+                transform: scale(0);
+            }
+        
+            to {
+                transform: scale(2);
+            }
+        }
+    `,
+    duration: 1000
+});
---
+++ effects/textIntro.js
@@ -0,0 +1,49 @@
+window._currentEffect = params => ({
+    html: /*html*/`
+        <svg width="100%" height="100%" style="display:none;">
+            <defs>
+                <filter id="wavy" filterUnits="userSpaceOnUse" x="0" y="0">
+                    <feTurbulence id="wave-animation" numOctaves="1" seed="1" baseFrequency="0 0.0645034">
+                    </feTurbulence>
+                    <feDisplacementMap scale="10" in="SourceGraphic"></feDisplacementMap>
+                </filter>
+                <animate 
+                    xlink:href="#wave-animation" 
+                    attributeName="baseFrequency" 
+                    dur="5s" keyTimes="0;0.5;1" 
+                    values="0.0 0.04;0.0 0.07;0.0 0.04"
+                    repeatCount="indefinite"></animate>
+            </defs>
+        </svg>
+        <span class="text">
+            ${params.text}
+        </span>
+    `,
+    css: /*css*/`
+        main .text {
+            font-size: 100px;
+            font-weight: bold;
+            filter: url("#wavy");
+        
+            /* -webkit-text-stroke: white 2px; */
+            color: black;
+            text-shadow: 0px 5px 0 white, 
+                0px -5px 0 white,
+                5px 0px 0 white, 
+                -5px 0px 0 white;
+        
+            animation: skew-text 2s infinite;
+        }
+        
+        @keyframes skew-text {
+            33% {
+                transform: skew(10deg);
+            }
+        
+            66% {
+                transform: skew(-10deg);
+            }
+        }
+    `,
+    duration: 2000
+});
