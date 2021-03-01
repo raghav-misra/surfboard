390c8adde01eefddb5dc4a52692bd05f80cd306a
Raghav Misra <raghav.m2014@gmail.com>
2021-03-01 11:34:44 -0800

    Listen for keypresses, bind "escape" to settings

--- js/effects.js
+++ js/effects.js
@@ -12,3 +12,24 @@ const _keys = (() => {
 
 const keyMap = Object.keys(_keys)
     .reduce((obj, key) => ({ ...obj, [_keys[key]]: key }), {});
+
+// Trigger effect megafunction:
+let effectInProgress = false;
+function triggerEffect(effect) {
+    
+}
+
+// Listen for key presses:
+document.body.addEventListener("keyup", e => {
+    const key = e.key.toLowerCase();
+
+    // Go to settings:q
+    if (key === "escape") {
+        location.href = "/settings.html";
+    }
+
+    else if (typeof keyMap[key] === "string" && !effectInProgress) {
+        console.log("Trigger effect:", keyMap[key]);
+        triggerEffect(keyMap[key]);
+    }
+});
